9fe4d18db8ae477de087f7cff06e72ecffdd6091
eduardockerse <eduardockerse@gmail.com>
2015-03-18 20:08:37 +0100

    reg ex... molan

--- app/src/main/java/com/inftel/isn/activity/CreatePostActivity.java
+++ app/src/main/java/com/inftel/isn/activity/CreatePostActivity.java
@@ -4,15 +4,24 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 import com.inftel.isn.R;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CreatePostActivity extends Activity {
     private final static String EMPTY_CONTENT_ALERT = "POST NEEDS TO HAVE SOME CONTENT...";
     private final static String CANT_POST_ALERT = "Can't post!";
+    private final static String INVALID_IMAGE_URL = "Image URL invalid";
+    private final static String INVALID_YOUTUBE = "Invalid Youtube link";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,29 +52,61 @@ public class CreatePostActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void selectMultimedia(){
-        // lanzar Dropbox
-        // seleccionar URL
-        // actuar sobre URL (filtrar, etc)
-        // guardar URL en campo correspondiente de CreatePostActvity
-    }
 
-    public void doPost(){
+    public void doPost(View view){
         EditText content = (EditText) findViewById(R.id.postContent);
+        EditText imageURL = (EditText) findViewById(R.id.imageURL);
+        EditText youtubeURL = (EditText) findViewById(R.id.youtube);
         String entryContent = content.getText().toString();
+        String image = imageURL.getText().toString();
+        String youtube = youtubeURL.getText().toString();
         if(!entryContent.isEmpty() && entryContent.length()>9) {
             //save post to database via async REST (on postExecute reload postList)
+            if(!image.isEmpty()) {
+                if((image.substring(0,3)).equalsIgnoreCase("www")) {
+                    // por si un usuario mete la URL sin http:// (si es https que lo ponga...)
+                    image = "http://"+image;
+                    Log.i("url-replaced",image);
+                }
 
+                try {
+                    URL url = new URL(image);
+                    //guardar el campo image
+                }
+                catch (MalformedURLException e) {
+                    //mandar campo image al carajo
+                    showDialog(CANT_POST_ALERT, INVALID_IMAGE_URL);
+                }
+            }
+            if(!youtube.isEmpty()) {
+                String vId = null;
+                Pattern pattern = Pattern.compile(".*(?:youtu.be\\/|v\\/|u\\/\\w\\/|embed\\/|watch\\?v=)([^#\\&\\?]*).*");
+                Matcher matcher = pattern.matcher(youtube);
+                if (matcher.matches()){
+                    vId = matcher.group(1);
+                }
+                if(vId != null){
+                    //guardar campo
+                }else{
+                    showDialog(CANT_POST_ALERT, INVALID_YOUTUBE);
+                }
+            }
         }else{
             //show alert dialog
-            new AlertDialog.Builder(this)
-                    .setTitle(CANT_POST_ALERT)
-                    .setMessage(EMPTY_CONTENT_ALERT)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int whichButton) {
-                        }
-                    }).show();
+            showDialog(CANT_POST_ALERT, EMPTY_CONTENT_ALERT);
         }
+    }
+
+
+    // mostrar mensaje de error
+    private void showDialog(String title, String message){
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                }).show();
     }
 }
