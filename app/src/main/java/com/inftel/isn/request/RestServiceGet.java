df0e5817d2d1e2e9a272582e114e3248dbe267dd
pedrojt1 <pj112358>
2015-03-22 18:46:28 +0100

    Maravilla de crearGrupos con BBDD

--- app/src/main/java/com/inftel/isn/request/RestServiceGet.java
+++ app/src/main/java/com/inftel/isn/request/RestServiceGet.java
@@ -9,71 +9,23 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
+public class RestServiceGet extends AsyncTask<String, Integer, JSONArray> {
 
-/**
- * Created by José on 20/03/2015.
- */
-public class RestServiceGet extends AsyncTask<String, Integer, String> {
-
-    protected String doInBackground(String... urls) {
-
-
-
-
+    protected JSONArray doInBackground(String... urls) {
         HttpClient httpClient = new DefaultHttpClient();
-
-        System.out.println("dir " + urls[0]);
-
-
         HttpGet g = new HttpGet(urls[0]);
         g.setHeader("Accept", "application/json");
         g.setHeader("Content-type", "application/json");
-        try
-        {
-
+        JSONArray respJSON = null;
+        try {
             HttpResponse resp = httpClient.execute(g);
             String respStr = EntityUtils.toString(resp.getEntity());
+            respJSON = new JSONArray(respStr);
 
-            int i;
-
-            JSONArray respJSON = new JSONArray(respStr);
-
-            if(respJSON.length() == 0)
-            {
-                System.out.println("inserto dato");
-                // otra llamada asincrona
-            }
-
-            return null;
-           /* for(i=0; i<respJSON.length(); i++){
-                JSONObject object = respJSON.getJSONObject(i);
-                String title = object.getString("title");
-                String entryContent = object.getString("description");
-                String lat = object.getString("latitude");
-                String lg = object.getString("longitude");
-                String im = object.getString("urlimage");
-                //datasource.open();
-                //datasource.createEntry(title, entryContent, lat, lg, im);
-                //datasource.close();
-            }*/
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
             Log.e("ServicioRest", "Error!", ex);
         }
-        return null;
-    }
-
-    protected void onProgressUpdate(Integer... progress){
-
-    }
-
-    protected void onPostExecute(String result) {
-        //reloadActivity();
+        return respJSON;
     }
 }
