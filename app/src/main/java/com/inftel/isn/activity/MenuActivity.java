d14b5d6de74903e53b3cb866e1c3f44d6e9a571c
loubna <bobolouna@gmail.com>
2015-03-24 19:50:16 +0100

    grupo

--- app/src/main/java/com/inftel/isn/activity/MenuActivity.java
+++ app/src/main/java/com/inftel/isn/activity/MenuActivity.java
@@ -132,6 +132,11 @@ public class MenuActivity extends FragmentActivity implements ActionBar.TabListe
                 startActivity(intent);
                 return true;
 
+            case R.id.buscarGroup:
+                intent = new Intent(this, SearchGroupsActivity.class);
+                startActivity(intent);
+                return true;
+
 
             default:
                 return super.onOptionsItemSelected(item);
